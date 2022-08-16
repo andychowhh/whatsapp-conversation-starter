bc79be5f914f433834354523764d4f74c9b08cec
Andy Chow <hohin523work@gmail.com>
2022-08-16 16:12:24 +0800

    Update tsconfig for absolute path, complete basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import "./App.css";
-import { Input } from "@chakra-ui/react";
+import { Box, Button, Input, InputGroup, InputLeftAddon } from "@chakra-ui/react";
+
+import { WHATSAPP_BASE_URL } from "constants/constants";
 
 function App() {
+  const [countryCode, setCountryCode] = useState<string>("852");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void =>
+    setPhoneNumber(event.target.value);
+
+  const onStartConversationButtonClick = (): void => {
+    window.open(`${WHATSAPP_BASE_URL}/${countryCode}${phoneNumber}`, '_blank');
+  };
+
   return (
-    <div className="App">
-      <Input placeholder="Basic usage" />
-    </div>
+    <Box className="App">
+      <InputGroup w="60%" m="auto">
+        <InputLeftAddon children={+`${countryCode}`}/>
+        <Input
+          type="tel"
+          placeholder="phone number"
+          value={phoneNumber}
+          onChange={handleInputChange}
+        />
+      </InputGroup>
+      <Button
+        colorScheme="teal"
+        size="md"
+        onClick={onStartConversationButtonClick}
+      >
+        Start Conversation
+      </Button>
+      <div>{phoneNumber}</div>
+    </Box>
   );
 }
 
---
+++ src/constants/constants.tsx
@@ -0,0 +1,1 @@
+export const WHATSAPP_BASE_URL = "https://wa.me"
