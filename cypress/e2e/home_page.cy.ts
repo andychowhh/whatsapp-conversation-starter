a772b07e2b20303fcb1fcf4bd9dc6e0086cb9495
Andy Chow <hohin523work@gmail.com>
2022-09-25 20:25:30 +0800

    Add new cypress test case

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -2,4 +2,20 @@ describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('/'); // change URL to match your dev URL
   });
+
+  it('block opening a new tab when there is no phone number input', () => {
+    // cy.visit('https://wa.me', {
+    //   onBeforeLoad(win) {
+    //     cy.stub(win, 'open-whatsapp-tab');
+    //   }
+    // });
+
+    cy.visit('/');
+
+    cy.get('input[placeholder="phone number"]').type('98765432');
+
+    cy.contains('Start Conversation').click();
+
+    // cy.window().its('open-whatsapp-tab').should('be.called');
+  });
 });
